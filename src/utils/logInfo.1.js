d5f030e588ec177197a51e441b1501e50671ae8c
Sagar Panchal <panchal.sagar@outlook.com>
2022-07-15 15:35:35 +0530

    delete extra file

--- src/utils/logInfo.1.js
+++
@@ -1,5 +0,0 @@
-import { isDevEnv } from './isDevEnv';
-
-export function logInfo(...args) {
-  return void (isDevEnv() || window.__DN?.DEBUG) && console.info(...args); // eslint-disable-line no-console
-}
